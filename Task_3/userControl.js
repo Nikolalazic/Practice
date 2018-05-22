6cbb718d7efe7f75f901e5af7d983a772e7f06cd
Nikolalazic <nikolalazic193@gmail.com>
2018-05-22 15:50:08 +0200

    add and remove method add

--- Task_3/userControl.js
+++ Task_3/userControl.js
@@ -1,29 +1,42 @@
 const arrayObj = require('./users');
 
 class UserControl {
-	constructor(userArray){
+	constructor(userArray) {
 		this.userArray = userArray;
 	}
 
-	findByIndex(index){
+	findByIndex(index) {
 		return this.userArray[index];
 	}
 
-	findById(id){
+	findById(id) {
 		const findUser = this.userArray.filter((user) => {
-			if(user.id === id){
-				return user ;
+			if (user.id === id) {
+				return user;
 			}
 		});
 		return findUser;
+	}
+
+	add(object) {
+		const users = this.userArray;
+		users.push(object);
+		console.log(users);
+	}
+	remove(index) {
+		const users = this.userArray;
+		const removeUser = users.splice(users[index], 1);
+		users.push(removeUser);
+		console.log(users);
 	}
 }
 
 const newArray = new UserControl(arrayObj);
 
 // console.log(newArray.findByIndex(2));
+//console.log(newArray.findById("user.deactivated"));
+//console.log(newArray.add("Neki novi id", "Nikola Lazic"));
 
-console.log(newArray.findById("user.deactivated"));
-
+console.log(newArray.remove(2));
 
 
