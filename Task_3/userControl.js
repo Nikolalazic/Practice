b8767132a7668b068e9e68b229f43bb9564ef816
Nikolalazic <nikolalazic193@gmail.com>
2018-05-24 12:31:39 +0200

    add update method

--- Task_3/userControl.js
+++ Task_3/userControl.js
@@ -5,29 +5,51 @@ class UserControl {
 		this.userArray = userArray;
 	}
 
+	findAll(object, limit) {
+		return this.userArray.filter((item, index) => {
+			const prepItem = item[Object.keys(object)[0]];
+			const prepObject = Object.values(object)[0];
+			if (index < limit) {
+				if (prepItem === prepObject) {
+					return item;
+				}
+			}
+		});
+	}
+
 	findByIndex(index) {
 		return this.userArray[index];
 	}
 
 	findById(id) {
-		const findUser = this.userArray.filter((user) => {
+		return this.userArray.filter((user) => {
 			if (user.id === id) {
 				return user;
 			}
 		});
-		return findUser;
 	}
 
 	add(object) {
-		const users = this.userArray;
-		users.push(object);
-		console.log(users);
+		this.userArray.push(object);
+		console.log(this.userArray);
 	}
+
+	update(index, newValue) {
+		if (index) {
+			const inputObject = this.userArray[index][Object.keys(newValue)] = Object.values(newValue)[0];
+
+			return this.userArray[index] = {
+				...this.userArray[index],
+				...inputObject
+			}
+		}
+
+	}
+
+
 	remove(index) {
-		const users = this.userArray;
-		const removeUser = users.splice(users[index], 1);
-		users.push(removeUser);
-		console.log(users);
+		const removeUser = this.userArray.splice(this.userArray[index], 1);
+		this.userArray.push(removeUser);
 	}
 }
 
@@ -36,7 +58,6 @@ const newArray = new UserControl(arrayObj);
 // console.log(newArray.findByIndex(2));
 //console.log(newArray.findById("user.deactivated"));
 //console.log(newArray.add("Neki novi id", "Nikola Lazic"));
-
-console.log(newArray.remove(2));
-
-
+//console.log(newArray.remove(4));
+//console.log(newArray.findAll({customerIo: "YES",}, 1));
+console.log(newArray.update(2, {"segment": "strina",}));
